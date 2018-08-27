157bce0ce70badb1f6386728be16c65c92650be3
FlMondon <florian.mondon@clermont.in2p3.fr>
2018-08-27 11:36:21 +0200

    first version of hf

--- hf_jla.py
+++ hf_jla.py
@@ -11,6 +11,9 @@ import cPickle as pkl
 from iminuit import Minuit, describe, Struct
 from mathplotlib import pyplot as plt
 from scipy.integrate import quad
+import glob
+import pyfits
+import types
 
 ###########
 #constants
@@ -198,19 +201,36 @@ class CosmoTools(object):
     		MU.append(self.dL_z(zz,zz,omgM,omgL,w)) 
     	return MU  
     
-
+    def Remove_Matrix(self, Tab,ID):
+        	"""
+        	function that remove from the 'Tab' matrix all the rows and colomns except those precised in 'ID'
+        	"""
+        	#create the list with the line to be removed
+        	try:
+        		tab = np.delete(np.arange(len(Tab[0])),ID,0)
+        	except:
+        		tab = np.delete(np.arange(len(Tab)),ID,0)
+        		
+        	#remove these lines to the original matrix
+        	Tab = np.delete(Tab,tab,0)
+        	try:
+        		Tab = np.delete(Tab,tab,1)
+        	except:
+        		'''nothing else to do'''
+        	return Tab
     
 class JLA_Hubble_fit(CosmoTools):
 
     """
     """
-    def __init__(self, path_params, path_cov):
+    def __init__(self, path_params, cov_path, sigma_mu_path):
         """
         """
-        
         self.path_params = path_params
-        self.path_cov = path_cov
-
+        self.cov_path = cov_path
+        self.sigma_mu_path = sigma_mu_path
+        
+        
     def read_JLA(self):
         """
         read JLA data (cf Marc Betoule 2014)
@@ -304,26 +324,110 @@ class JLA_Hubble_fit(CosmoTools):
             Cstat[i*3 +1,i*3 +2]= self.cov_sc[i]
             Cstat[i*3 +2,i*3 +1]= self.cov_sc[i]
             Cstat[i*3+2,i*3 +2]= self.dC[i]**2         
-        return Cstat
+        self.Cstat = Cstat
 
-
+    def mu_cov(self, alpha, beta):
+        """
+        #Function that buil the covariance matrix as Betoule et al (2014) betzeen SNe to get the chi2
+        imputs:
+        -alpha: free parameter of the Hubble fit (factor of the stretch)
+        -beta: free parameter of the Hubble fit (factor of the color)
+        -IDJLA: is an array containing the indexing value of each SNe
+        outuput:
+        -Cmu : the covariance matrix (2 dimension array)
+        cov_path: path to covariance matrix or array corespond directly to the covariance matrix itself
+        sigma_mu_path : path to sigma_mu.txt
+        """
+        #Assemble the full covariance matrix of distance modulus, See Betoule et al. (2014), Eq. 11-13 for reference
+        #You have to acces the data which are in '/data/software/jla_likelihood_v6/covmat/C*.fits'. The C_hosts.fits has been removed from the analysis  
+        #Ceta = sum([pyfits.getdata(mat) for mat in glob.glob('Data/covmat/C*.fits')])
+    
+        #test if the covariance matrix come from a file or it already a matrix
+        if type(self.cov_path) is types.StringType:
+            Ceta = sum([pyfits.getdata(mat) for mat in glob.glob(self.cov_path)])
+            
+            
+    #        Ceta += sum([pyfits.getdata(mat) for mat in glob.glob('/data/software/jla_likelihood_v6/covmat/C_cal.fits')])
+        else:
+            Ceta = self.cov_path
+        if type(self.sigma_mu_path) is types.StringType:
+            sigma = np.loadtxt(self.sigma_mu_path)
+        else:
+            sigma= self.sigma_mu_path
+    
+        Cmu = np.zeros_like(Ceta[::3,::3])
+        for i, coef1 in enumerate([1., alpha, -beta]):
+            for j, coef2 in enumerate([1., alpha, -beta]):
+                Cmu += (coef1 * coef2) * Ceta[i::3,j::3]
+        # Add diagonal term from Eq. 13
+    #    print(len(Cmu[:,0]))
+        sigma_pecvel = (5 * 150 / 3e5) / (np.log(10.) * sigma[:, 2])
+        Cmu[np.diag_indices_from(Cmu)] += sigma_pecvel ** 2 + sigma[:, 0] ** 2 + sigma[:, 1] ** 2  
+    #    Cmu=N.diag(Cmu[N.diag_indices_from(Cmu)])
+        self.Cmu = self.Remove_Matrix(Cmu,self.IDJLA)
+        
+    
 	def chi2(self,omgM,omgL,w,alpha,beta,Mb,delta_M):
          ''' Funtion that calculate the chi2 '''
          result=0.
          if alpha!=self.cache_alpha or beta!=self.cache_beta:
-             self.Mat = inv(mu_cov(alpha,beta,self.IDJLA, self.cov_path, self.sigma_mu_path))
-             self.cache_alpha=alpha
-             self.cache_beta=beta
-         mu_z=muexp(self.mB,self.X1,self.C,alpha,beta,Mb,delta_M,self.M_stell)
+             self.Mat = np.linalg.inv(self.mu_cov(alpha,beta))
+             self.cache_alpha = alpha
+             self.cache_beta = beta
+         mu_z = self.muexp(self.mB,self.X1,self.C,alpha,beta,Mb,delta_M,self.M_stell)
 
          #loop for matrix construction
          for i in range(len(self.zcmb)):
-             zz = self.zcmb[i]
-             zzz = self.zhel[i]
-           
-             self.dL[i] = self.dL_z(self.fixe,zz,zzz,omgM,omgL,w)
+             zz = self.zcmb[i]           
+             self.dL[i] = self.dL_z(self.fixe,zz,zz,omgM,omgL,w)
 
          #contruction of the chi2 by matrix product
          result =  np.dot( (mu_z-self.dL), np.dot((self.Mat),(mu_z-self.dL)))
-         self.chi2tot = result
          return result
+
+    def Hubble_diagram(self):
+        """
+        Function which make the hubble fit of the given compilation.
+        inputs :
+        	-omgM: 1st free parameter to be fitted initialized to the 0.295 value if not precised
+        	-alpha: 2nd free parameter to be fitted initialized to the 0.141 value if not precised
+        	-beta: 3rd free parameter to be fitted initialized to the 3.101 value if not precised
+        	-Mb: 4th free parameter to be fitted initialized to the -19.05 value if not precised
+        	-delta_M: 5th free parameter to be fitted initialized to the -0.070 value if not precised
+          -fixe: when fixe=0 omgL and w are fixe, fixe = 1 w is fixe, fixe =2 omgL is fixe else all parameters are fixe
+        	-zcmb: an array which contains the redshifts of the SNs
+        	-mB: B band peak magnitude of the SNs
+        	-dmB: uncertainty on mB
+        	-X1:  SALT2 shape parameter
+        	-dX1: uncertainty on X1
+        	-dC: uncertainty on C	
+        	-C: colour parameter
+        	-M_stell: the log_10 host stellar mass in units of solar mass
+        	-IDJLA: index of the SNs from the 740 of jla used for the fit
+        	-results : a file where some results wiil be written
+        	- m : the iminuit object (see doc of iminuit for more information).
+        	-ecarts5 : the residuals of the fit
+        """
+        #check : need to have at least 2 SNe
+        '''f2,(test,P2,P3,P4,P5) = P.subplots(5, sharex=True, sharey=False, gridspec_kw=dict(height_ratios=[3,1,1,1,1]))'''
+        
+        
+        #minimisation of the chi2
+        '''
+        the values of the free parameter can be initialized.
+        '''
+      
+        if self.model == 0 :
+    #        m=Minuit(chi2mini.chi2,omgM=0.295,omgL=N.nan,w=-1,alpha=0.141,beta=3.101,Mb=-19.05,delta_M=-0.070,limit_omgM=(0.2,0.4),limit_omgL=(-1,1),limit_w=(-1.4,0.4),limit_alpha=(0.1,0.2),limit_beta=(2.0,4.0),limit_Mb=(-20.,-18.),limit_delta_M=(-0.1,-0.0),fix_omgM=False,fix_omgL=True,fix_w=True, fix_alpha=False, fix_beta=False, fix_Mb=False, fix_delta_M=False, print_level=1)    
+            m=Minuit(self.chi2,omgM=0.2,omgL=np.nan,w=-1,alpha=0.141,beta=3.101,Mb=-19.05,delta_M=-0.070,limit_omgM=(0.2,0.4),limit_omgL=(-1,1),limit_w=(-1.4,0.4),limit_alpha=(0.1,0.2),limit_beta=(2.0,4.0),limit_Mb=(-20.,-18.),limit_delta_M=(-0.1,-0.0),fix_omgM=False,fix_omgL=True,fix_w=True, fix_alpha=False, fix_beta=False, fix_Mb=False, fix_delta_M=False, print_level=1)    
+        elif self.model ==1 :
+    #        last_iter_args={u'fix_Mb': True, u'limit_w': None, u'fix_delta_M': True, u'error_omgL': 0.1672842193144214, u'error_omgM': 0.11284796124765262, 'delta_M': -0.07040462309885408, u'error_Mb': 0.02559618467415318, u'error_w': 1.0, u'error_alpha': 0.006593439955397084, u'fix_alpha': True, u'limit_alpha': None, 'Mb': -19.038807526119403, u'limit_omgM': None, u'limit_omgL': None, u'limit_Mb': None, 'beta': 3.0999314004965535, u'limit_delta_M': None, u'limit_beta': None, 'alpha': 0.14098769528058613, 'omgM': 0.20020038670359183, 'omgL': 0.561065865545341, u'fix_w': True, u'fix_beta': True, u'fix_omgM': False, u'fix_omgL': False, u'error_delta_M': 0.023111327883836057, u'error_beta': 0.08066293365248206, 'w': -1.0}
+    #        m=Minuit(chi2mini.chi2,**last_iter_args)           
+            m=Minuit(self.chi2,omgM=0.2,omgL=0.55,w=-1,alpha=0.141,beta=3.101,Mb=-19.05,delta_M=-0.070,fix_omgM=False,fix_omgL=False,fix_w=True, fix_alpha=False, fix_beta=False, fix_Mb=False, fix_delta_M=False, print_level=1)    
+        elif self.model ==2 :    
+            m=Minuit(self.chi2,omgM=0.2,omgL=np.nan,w=-1,alpha=0.141,beta=3.101,Mb=-19.05,delta_M=-0.070,fix_omgM=False,fix_omgL=True,fix_w=False, fix_alpha=False, fix_beta=False, fix_Mb=False, fix_delta_M=False, print_level=1)    
+        else :
+            m=Minuit(self.chi2,omgM=0,omgL=0.5611,w=-1,alpha=0.141,beta=3.101,Mb=-19.05,delta_M=-0.070,limit_omgM=(0,0.4),limit_omgL=(-1,1),limit_w=(-1.4,0.4),limit_alpha=(0.1,0.2),limit_beta=(2.0,4.0),limit_Mb=(-20.,-18.),limit_delta_M=(-0.1,-0.0),fix_omgM=True,fix_omgL=True,fix_w=True, fix_alpha=False, fix_beta=False, fix_Mb=False, fix_delta_M=False, print_level=1)    
+        
+        m.migrad()
+        return  m
